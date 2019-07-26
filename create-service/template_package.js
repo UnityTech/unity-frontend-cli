d31b150075658e015185ac146dcf1af67cb1f564
Andy Jepkes <andrewje@unity3d.com>
2019-07-26 15:47:09 -0700

    update dev-script

--- create-service/template_package.js
+++ create-service/template_package.js
@@ -29,7 +29,7 @@ const contents = (options) => `{
   },
   "author": "Unity Technologies",
   "devDependencies": {
-    "@packagedcomponents/dev-scripts": "^1.2.15",
+    "@packagedcomponents/dev-scripts": "^1.3.23",
     "@babel/core": "^7.2.2",
     "@babel/plugin-proposal-class-properties": "^7.3.0",
     "@babel/preset-env": "^7.3.1",
